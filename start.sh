539f51eaea8d2974756cd3b09095e39cd97c8ad7
mstrpupt2 <seancorkum@yahoo.com>
2017-01-22 09:50:33 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 
-CWD='opt/mattermost/bin'
-cd $CWD
-./platform
+config=/mattermost/config/config.json
+DB_HOST=${DB_HOST:-db}
+DB_PORT_5432_TCP_PORT=${DB_PORT_5432_TCP_PORT:-5432}
+MM_USERNAME=${MM_USERNAME:-docker}
+MM_PASSWORD=${MM_PASSWORD:-mmuser_password}
+MM_DBNAME=${MM_DBNAME:-mattermost}
+echo -ne "Configure database connection..."
+if [ ! -f $config ]
+then
+    cp /config.template.json $config
+    sed -Ei "s/DB_HOST/$DB_HOST/" $config
+    sed -Ei "s/DB_PORT/$DB_PORT_5432_TCP_PORT/" $config
+    sed -Ei "s/MM_USERNAME/$MM_USERNAME/" $config
+    sed -Ei "s/MM_PASSWORD/$MM_PASSWORD/" $config
+    sed -Ei "s/MM_DBNAME/$MM_DBNAME/" $config
+    echo OK
+else
+    echo SKIP
+fi
+
+echo "Wait until database $DB_HOST:$DB_PORT_5432_TCP_PORT is ready..."
+until nc -z $DB_HOST $DB_PORT_5432_TCP_PORT
+do
+    sleep 1
+done
+
+# Wait to avoid "panic: Failed to open sql connection pq: the database system is starting up"
+sleep 1
+
+echo "Starting platform"
+cd /mattermost/bin
+./platform $*
